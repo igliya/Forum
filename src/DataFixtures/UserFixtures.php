e51093c51799512003a4f809906255e7570ca9df
Angelina Y <l4195562@gmail.com>
2021-04-24 23:08:58 +0300

    Update fixtures passwords

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,14 +20,14 @@ class UserFixtures extends Fixture
     {
         // create user
         $user = new User();
-        $user->setPassword($this->passwordEncoder->encodePassword($user, 'user123!'));
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'User123!'));
         $user->setRoles(['ROLE_USER']);
         $user->setEmail('user@example.com');
         $user->setName('Пользователь');
         $manager->persist($user);
         // create admin
         $admin = new User();
-        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'admin123!'));
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'Admin123!'));
         $admin->setRoles(['ROLE_ADMIN']);
         $admin->setEmail('admin@example.com');
         $admin->setName('Администратор');
