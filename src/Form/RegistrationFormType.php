782e7a362837c803e4844fe52b5ff7f883eeed45
Angelina Y <l4195562@gmail.com>
2021-04-24 23:08:20 +0300

    Make register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,42 +5,85 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email([
+                        'message' => 'Некорректный email'
+                    ]),
+                    new Length([
+                        'max' => 255
+                    ])
+                ],
+                'attr' => [
+                    'maxlength' => 255,
+                    'placeholder' => 'Введите email'
+                ]
+            ])
+            ->add('name', null, [
+                'constraints' => [
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Имя должно быть не менее {{ limit }} символов',
+                        'max' => 255
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => 'Введите имя'
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Пароль должен быть не менее {{ limit }} символов',
+                        'max' => 255
+                    ]),
+                    new Regex([
+                        'pattern' => '/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/',
+                        'message' => 'Пароль должен состоять из латинских заглавных и строчных букв, содержать цифры и символы !@#$%^&*'
+                    ])
+                ],
+                'first_options'  => [
+                    'attr' => [
+                        'maxlength' => 255,
+                        'placeholder' => 'Введите пароль'
+                    ]
+                ],
+                'second_options' => [
+                    'attr' => [
+                        'maxlength' => 255,
+                        'placeholder' => 'Повторите пароль'
+                    ]
+                ],
+                'invalid_message' => 'Пароли не совпадают'
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'required' => true,
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Вы должны согласиться на обработку персональных данных'
                     ]),
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                'label' => 'Согласие на обработку персональных данных'
             ])
         ;
     }
