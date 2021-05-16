52dda7cd3741261996ec51d0203c7ade3f2cc8b2
Angelina Y <l4195562@gmail.com>
2021-05-16 16:26:22 +0300

    Updated AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,15 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
+use App\Entity\Section;
+use App\Entity\Topic;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixtures extends Fixture
+class AppFixtures extends Fixture
 {
     private $passwordEncoder;
 
@@ -18,13 +21,19 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        // create user
-        $user = new User();
-        $user->setPassword($this->passwordEncoder->encodePassword($user, 'User123!'));
-        $user->setRoles(['ROLE_USER']);
-        $user->setEmail('user@example.com');
-        $user->setName('Пользователь');
-        $manager->persist($user);
+        // create users
+        // create user1
+        $user1 = new User();
+        $user1->setPassword($this->passwordEncoder->encodePassword($user1, 'User1!123'));
+        $user1->setEmail('user1@example.com');
+        $user1->setName('Пользователь 1');
+        $manager->persist($user1);
+        // create user2
+        $user2 = new User();
+        $user2->setPassword($this->passwordEncoder->encodePassword($user2, 'User2!123'));
+        $user2->setEmail('user2@example.com');
+        $user2->setName('Пользователь 2');
+        $manager->persist($user2);
         // create admin
         $admin = new User();
         $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'Admin123!'));
@@ -32,7 +41,33 @@ class UserFixtures extends Fixture
         $admin->setEmail('admin@example.com');
         $admin->setName('Администратор');
         $manager->persist($admin);
-        // flush manager
+        // create sections
+        $sections = ['animals' => 'Животные', 'books' => 'Книги', 'medicine' => 'Медицина', 'sport' => 'Спорт',
+            'tv-and-cinema ' => 'ТВ и кино', 'photos' => 'Фото', 'computer-science' => 'Компьютерные технологии'];
+        foreach ($sections as $code => $name) {
+            $section = new Section();
+            $section->setCode($code);
+            $section->setName($name);
+            $manager->persist($section);
+            $topic = new Topic();
+            $topic->setSection($section);
+            $topic->setAuthor($admin);
+            $topic->setTitle('Правила оформления топиков');
+            $topic->setText('Правила оформления топиков');
+            $manager->persist($topic);
+            // first comment
+            $comment1 = new Comment();
+            $comment1->setTopic($topic);
+            $comment1->setAuthor($user1);
+            $comment1->setText('Хорошие правила');
+            $manager->persist($comment1);
+            // second comment
+            $comment2 = new Comment();
+            $comment2->setTopic($topic);
+            $comment2->setAuthor($user2);
+            $comment2->setText('Полностью согласен с правилами!');
+            $manager->persist($comment2);
+        }
         $manager->flush();
     }
 }
