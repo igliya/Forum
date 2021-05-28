a8fd7341bcc409e296f4f73a38d0812241db7bca
Angelina Y <l4195562@gmail.com>
2021-05-29 01:31:44 +0300

    Sort section by name

--- src/Repository/SectionRepository.php
+++ src/Repository/SectionRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Section|null find($id, $lockMode = null, $lockVersion = null)
  * @method Section|null findOneBy(array $criteria, array $orderBy = null)
- * @method Section[]    findAll()
  * @method Section[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class SectionRepository extends ServiceEntityRepository
@@ -17,6 +16,14 @@ class SectionRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Section::class);
+    }
+
+    public function findAll()
+    {
+        return $this->createQueryBuilder('s')
+            ->orderBy('s.name', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
     public function findByCode($value): ?Section
