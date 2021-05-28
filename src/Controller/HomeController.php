7d03cd8b03d01fa130523518082aa0ea86a4e029
Angelina Y <l4195562@gmail.com>
2021-05-29 02:43:47 +0300

    Added topics paginator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\SectionRepository;
 use App\Repository\TopicRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +16,32 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(SectionRepository $sectionRepository, TopicRepository $topicRepository): Response
-    {
+    public function index(
+        SectionRepository $sectionRepository,
+        TopicRepository $topicRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        // get topics query
+        $topicsQuery = $topicRepository->createQueryBuilder('t')
+            ->orderBy('t.createdDate', 'DESC')
+            ->getQuery()
+            ;
+        // get page number
+        $page_number = $request->query->getInt('page', 1);
+        // set correct page number
+        if ($page_number < 1) {
+            $page_number = 1;
+        }
+        // paginate
+        $pagination = $paginator->paginate(
+            $topicsQuery,
+            $page_number,
+            10
+        );
+
         return $this->render('home/index.html.twig', [
-            'topics' => $topicRepository->findAll(),
+            'pagination' => $pagination,
             'sections' => $sectionRepository->findAll()
         ]);
     }
@@ -27,6 +51,8 @@ class HomeController extends AbstractController
      */
     public function sections(
         TopicRepository $topicRepository,
+        PaginatorInterface $paginator,
+        Request $request,
         SectionRepository $sectionRepository,
         string $sectionCode
     ): Response {
@@ -42,10 +68,30 @@ class HomeController extends AbstractController
         if ($section === null) {
             throw new NotFoundHttpException();
         }
+        // get topics query
+        $topicsQuery = $topicRepository->createQueryBuilder('t')
+            ->andWhere('t.section = :val')
+            ->setParameter('val', $section)
+            ->orderBy('t.createdDate', 'DESC')
+            ->getQuery()
+            ;
+        // get page number
+        $page_number = $request->query->getInt('page', 1);
+        // set correct page number
+        if ($page_number < 1) {
+            $page_number = 1;
+        }
+        // paginate
+        $pagination = $paginator->paginate(
+            $topicsQuery,
+            $page_number,
+            10
+        );
+
         return $this->render('section/show.html.twig', [
             'section' => $section,
             'sections' => $sections,
-            'topics' => $topicRepository->findBySection($section)
+            'pagination' => $pagination
         ]);
     }
 }
--- src/Repository/TopicRepository.php
+++ src/Repository/TopicRepository.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Topic|null find($id, $lockMode = null, $lockVersion = null)
  * @method Topic|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Topic[]    findAll()
  * @method Topic[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class TopicRepository extends ServiceEntityRepository
@@ -16,23 +17,5 @@ class TopicRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Topic::class);
-    }
-
-    public function findAll()
-    {
-        return $this->createQueryBuilder('t')
-            ->orderBy('t.createdDate', 'DESC')
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function findBySection($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.section = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.createdDate', 'DESC')
-            ->getQuery()
-            ->getResult();
     }
 }
